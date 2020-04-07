d7295fbfe3a73a59aa7a39f8d4232883bbb92c57
Gaurav6342 <48625801+Gaurav6342@users.noreply.github.com>
2020-04-07 09:30:46 +0530

    Add files via upload

---
+++ cvv.py
@@ -0,0 +1,17 @@
+import cv2
+cap = cv2.VideoCapture(0)               # image capture function with camera no as attribute
+
+while True:
+
+     status , photo = cap.read()          #click the photo function
+     cv2.imwrite('gaurav.png',photo)     #save the photo to python program where to save after esc button press
+     cv2.imshow('my image',photo)        # open the photo
+     if cv2.waitKey(1) == 27:            #wait for 1 sec or 27 is no for esc (when enter esc window close)
+              break
+
+
+
+cv2.destroyAllWindows()                #window destoy after esc button
+cap.release()
+
+
